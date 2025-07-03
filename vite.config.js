aeb8661b65a5f31656163a6608a627acdd2372f5
Saki Aihara <sakiazaele@gmail.com>
2025-07-03 14:28:43 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    base: '/', // ← penting! tanpa ini path asset bisa kacau di hosting
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
